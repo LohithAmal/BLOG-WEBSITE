51ed1ab6e8885e30afee33ab786022dfbdf46fdb
lohith <lohith05amal@gmail.com>
2022-01-26 23:40:40 -0500

    hero image

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import HomeImage from "../Assets/home-2.jpg";
 import "./Header.css";
 export default function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="header-title">
+        <span className="header-title-sm">React & Node</span>
+        <span className="header-title-lg">Blog</span>
+      </div>
+      <img className="header-image" src={HomeImage} alt="home image"></img>
+    </div>
+  );
 }
